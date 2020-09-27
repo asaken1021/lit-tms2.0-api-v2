528897da6029f77a8295f184b56bbb74fe325937
asaken1021 <asaken1021@outlook.com>
2020-09-27 13:35:36 +0900

    [add] JWT gem, API implementation

--- app.rb
+++ app.rb
@@ -24,14 +24,54 @@ before do
   response.headers["Access-Control-Allow-Credentials"] = "true"
 end
 
-helpers do
-  def current_user
-    User.find_by(id: session[:user])
-  end
+def bad_request
+  status 400
+  res_data = {
+    error: 'Bad Request'
+  }
+
+  return json res_data.to_json
 end
 
-get '/' do
-  'Test'
+def unauthorized
+  status 401
+  res_data = {
+    error: 'Unauthorized'
+  }
+
+  return json res_data.to_json
+end
+
+def forbidden
+  status 403
+  res_data = {
+    error: 'Forbidden'
+  }
+
+  return json res_data.to_json
+end
+
+def not_found
+  status 404
+  res_data = {
+    error: 'Not Found'
+  }
+
+  return json res_data.to_json
+end
+
+def update_project_progress(id = nil)
+  project = Project.find_by(id: id)
+  all_tasks = Task.where(project_id: id)
+  completed_tasks = all_tasks.where(progress: 100)
+
+  if all_tasks.count == 0
+    project.progress = 0
+  else
+    project.progress = (completed_tasks.count.to_f / all_tasks.count.to_f * 100)
+  end
+
+  project.save
 end
 
 namespace '/api' do
@@ -40,7 +80,12 @@ namespace '/api' do
 
     before do
       request.body.rewind
-      req_data = JSON.parse(request.body.string)
+
+      if request.body.string == ""
+        req_data = JSON.parse("{}")
+      else
+        req_data = JSON.parse(request.body.string)
+      end
     end
 
     post '/users' do
@@ -51,52 +96,31 @@ namespace '/api' do
         password_confirmation: req_data["password_confirmation"],
         line_id: ""
       )
-      if user.persisted?
-        status 200
-        payload = {
-          id: user.id
-        }
-        token = JWT.encode(payload, privkey, 'RS256')
-        res_data = {
-          token: token
-        }
-      else
-        if req_data["password"] != req_data["password_confirmation"]
-          status 400
-          res_data = {
-            error: 'Bad Request: PASSWORD_MISMATCH'
-          }
-        else
-          status 400
-          res_data = {
-            error: 'Bad Request: UNKNOWN_ERROR'
-          }
-        end
-      end
+      return bad_request if !user.persisted?
+
+      status 200
+      payload = {
+        id: user.id
+      }
+      token = JWT.encode(payload, privkey, 'RS256')
+      res_data = {
+        token: token
+      }
 
       json res_data.to_json
     end
 
     get '/users/:id' do
-      if params[:id] != nil
-        user = User.find_by(id: params[:id])
-        if user != nil
-          status 200
-          res_data = {
-            user: user
-          }
-        else
-          status 404
-          res_data = {
-            error: 'Not Found'
-          }
-        end
-      else
-        status 400
-        res_data = {
-          error: 'Bad Request'
-        }
-      end
+      return bad_request if params[:id] == nil
+
+      user = User.find_by(id: params[:id])
+      return not_found if user == nil
+
+      status 200
+      res_data = {
+        user: user
+      }
+
       json res_data.to_json
     end
 
@@ -106,21 +130,17 @@ namespace '/api' do
 
     post '/session' do
       user = User.find_by(mail: req_data["mail"])
-      if user && user.authenticate(req_data["password"])
-        status 200
-        payload = {
-          id: user.id
-        }
-        token = JWT.encode(payload, privkey, 'RS256')
-        res_data = {
-          token: token
-        }
-      else
-        status 400
-        res_data = {
-          error: 'Bad Request'
-        }
-      end
+      return bad_request if user == nil
+      return bad_request if !user.authenticate(req_data["password"])
+
+      status 200
+      payload = {
+        id: user.id
+      }
+      token = JWT.encode(payload, privkey, 'RS256')
+      res_data = {
+        token: token
+      }
 
       json res_data.to_json
     end
@@ -130,95 +150,61 @@ namespace '/api' do
     end
 
     get '/projects' do
-      if req_data["user_id"] != nil
-        if User.find_by(id: req_data["user_id"]) != nil
-          status 200
-          projects = Project.find_by(user_id: req_data["user_id"])
-          res_data = {
-            projects: projects
-          }
-        else
-          status 404
-          res_data = {
-            error: 'Not Found'
-          }
-        end
-      else
-        status 400
-        res_data = {
-          error: 'Bad Request'
-        }
-      end
+      token = req_data["token"]
+      return bad_request if token == nil
+
+      user_id = JWT.decode(token, pubkey, false, { algorithm: 'RS256' })[0]["id"]
+      return bad_request if user_id == nil
+
+      user = User.find_by(id: user_id)
+      return unauthorized if user == nil
+
+      projects = Project.find_by(user_id: user.id)
+      return not_found if projects == nil
+
+      status 200
+      res_data = {
+        projects: projects
+      }
 
       json res_data.to_json
     end
 
     post '/projects' do
       token = req_data["token"]
-      if token != nil
-        binding.pry
-        user_id = JWT.decode(token, pubkey, false, { algorithm: 'RS256' })[0]["id"]
-        if user_id != nil
-          user = User.find_by(id: user_id)
-          if user != nil
-            project = Project.create(
-              name: req_data["name"],
-              progress: 0,
-              user_id: user_id
-            )
-            if project != nil
-              status 200
-              res_data = {
-                project: project
-              }
-            else
-              status 400
-              res_data = {
-                error: 'Bad Request'
-              }
-            end
-          else
-            status 401
-            res_data = {
-              error: 'Unauthorized'
-            }
-          end
-        else
-          status 400
-          res_data = {
-            error: 'Bad Request'
-          }
-        end
-      else
-        status 400
-        res_data = {
-          error: 'Bad Request'
-        }
-      end
+      return bad_request if token == nil
+
+      user_id = JWT.decode(token, pubkey, false, { algorithm: 'RS256' })[0]["id"]
+      return bad_request if user_id == nil
+
+      user = User.find_by(id: user_id)
+      return unauthorized if user == nil
+
+      project = Project.create(
+        name: req_data["name"],
+        progress: 0,
+        user_id: user_id
+      )
+      return bad_request if project == nil
+
+      status 200
+      res_data = {
+        project: project
+      }
 
       json res_data.to_json
     end
 
     get '/projects/:id' do
+      return bad_request if params[:id] == nil
+
       project = Project.find_by(id: params[:id])
-      if project != nil
-        status 200
-        res_data = {
-          project: project
-        }
-      else
-        if params[:id] == nil
-          status 400
-          res_data = {
-            error: 'Bad Request'
-          }
-        else
-          status 404
-          res_data = {
-            error: 'Not Found'
-          }
-        end
-      end
+      return not_found if project == nil
+
+      status 200
+      res_data = {
+        project: project
+      }
 
       json res_data.to_json
     end
@@ -228,19 +214,131 @@ namespace '/api' do
     end
 
     get '/phases' do
-      status 501
+      return bad_request if req_data["project_id"] == nil
+
+      phases = Phase.find_by(project_id: req_data["project_id"])
+      return not_found if phases == nil
+
+      status 200
+      res_data = {
+        phases: phases
+      }
+
+      json res_data.to_json
     end
 
     post '/phases' do
+      token = req_data["token"]
+      return bad_request if token == nil
+
+      user_id = JWT.decode(token, pubkey, false, { algorithm: 'RS256' })[0]["id"]
+      return unauthorized if user_id == nil
+
+      return bad_request if req_data["project_id"] == nil
+
+      project = Project.find_by(id: req_data["project_id"])
+      return not_found if project == nil
+      return forbidden if user_id != project.user_id
+
+      deadline_date = req_data["deadline"].split('-')
+      return bad_request if deadline_date == nil
+      return bad_request if !Date.valid_date?(deadline_date[0].to_i, deadline_date[1].to_i, deadline_date[2].to_i)
+      return bad_request if req_data["name"] == nil
+
+      phase = Phase.create(
+        name: req_data["name"],
+        deadline: req_data["deadline"],
+        project_id: project.id
+      )
+      status 200
+      res_data = {
+        phase: phase
+      }
+
+      json res_data.to_json
+    end
+
+    put '/phases/:id' do
       status 501
     end
 
     get '/tasks' do
-      status 501
+      return bad_request if req_data["project_id"] == nil
+
+      tasks = Task.find_by(project_id: req_data["project_id"])
+      return not_found if tasks == nil
+
+      status 200
+      res_data = {
+        tasks: tasks
+      }
+
+      json res_data.to_json
     end
 
     post '/tasks' do
-      status 501
+      token = req_data["token"]
+      return bad_request if token == nil
+
+      user_id = JWT.decode(token, pubkey, false, { algorithm: 'RS256' })[0]["id"]
+      return unauthorized if user_id == nil
+
+      return bad_request if req_data["project_id"] == nil
+      return bad_request if req_data["phase_id"] == nil
+
+      project = Project.find_by(id: req_data["project_id"])
+      phase = Phase.find_by(id: req_data["phase_id"])
+      return not_found if project == nil
+      return not_found if phase == nil
+      return forbidden if user_id != project.user_id
+      return bad_request if project.id != phase.project_id
+
+      task = Task.create(
+        name: req_data["name"],
+        memo: req_data["memo"],
+        progress: 0,
+        phase_id: phase.id,
+        project_id: project.id
+      )
+      return bad_request if task == nil
+
+      update_project_progress(project.id)
+
+      status 200
+      res_data = {
+        task: task
+      }
+
+      json res_data.to_json
+    end
+
+    put '/tasks/:id' do
+      token = req_data["token"]
+      return bad_request if token == nil
+
+      user_id = JWT.decode(token, pubkey, false, { algorithm: 'RS256' })[0]["id"]
+      return unauthorized if user_id == nil
+
+      return bad_request if params[:id] == nil
+
+      task = Task.find_by(id: params[:id])
+      return not_found if task == nil
+
+      project = Project.find_by(id: task.project_id)
+
+      return forbidden if user_id != project.user_id
+
+      task.progress = req_data["task_progress"]
+      task.save
+
+      update_project_progress(project.id)
+
+      status 200
+      res_data = {
+        task: task
+      }
+
+      json res_data.to_json
     end
   end
 end
